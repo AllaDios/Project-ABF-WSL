ef3af5b80c5cce3e13d31134244b1e1b9a506a27
AllaDios <lautaroalladio12@gmail.com>
2024-11-13 11:59:30 -0300

    move main to root

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import pyqtSlot
-from menu import MainWindow
+from src.menu import MainWindow
 
 """
 from Archivo convertido con pyside2-uic archivo.ui > interfaz.py
