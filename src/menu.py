1be9d339b249199360d44dc34333565b193c54fe
AllaDios <lautaroalladio12@gmail.com>
2024-11-13 12:07:35 -0300

    parchadito wacho

--- src/menu.py
+++ src/menu.py
@@ -3,7 +3,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import pyqtSlot
 from interfaces.ui_menu import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
